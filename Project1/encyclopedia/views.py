699e2d8b550e62a4e77f38880a30dbb127f1a52f
Aimee Ha <aimee.ha95@gmail.com>
2022-07-14 23:45:31 +0100

    search done

--- Project1/encyclopedia/views.py
+++ Project1/encyclopedia/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render
+from tkinter import Entry
+from django.shortcuts import redirect, render
 
 from . import util
 
 import markdown2
 
-entries = util.list_entries()
-
 
 def index(request):
+    entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {"entries": entries})
 
 
 def title(request, title):
+    entries = util.list_entries()
     up_entries = [entry.upper() for entry in entries]
     if title.upper() in up_entries:
         content = markdown2.markdown(util.get_entry(title))
@@ -22,3 +23,13 @@ def title(request, title):
         )
     else:
         return render(request, "encyclopedia/error.html")
+
+
+def search(request):
+    entries = util.list_entries()
+    query = request.GET.get("q")
+    if query.upper() in [entry.upper() for entry in entries]:
+        return redirect("wiki/" + query)
+    else:
+        results = [entry for entry in entries if query.upper() in entry.upper()]
+        return render(request, "encyclopedia/index.html", {"entries": results})
