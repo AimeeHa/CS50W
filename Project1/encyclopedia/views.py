03c2054e35fc0492c6e3e6f3b96e13957855490b
Aimee Ha <aimee.ha95@gmail.com>
2022-07-14 22:05:14 +0100

    entry page done

--- Project1/encyclopedia/views.py
+++ Project1/encyclopedia/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def title(request, title):
+    entries = util.list_entries()
+    up_entries = [entry.upper() for entry in entries]
+    if title.upper() in up_entries:
+        content = markdown2.markdown(util.get_entry(title))
+        return render(
+            request,
+            "encyclopedia/titles.html",
+            {"content": content},
+        )
+    else:
+        return render(request, "encyclopedia/error.html")
