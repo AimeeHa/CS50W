39465f9e639ea085cc2534847708393c3ec2b3b4
Aimee Ha <aimee.ha95@gmail.com>
2022-07-14 22:16:39 +0100

    index page updated

--- Project1/encyclopedia/views.py
+++ Project1/encyclopedia/views.py
@@ -4,13 +4,14 @@ from . import util
 
 import markdown2
 
+entries = util.list_entries()
+
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+    return render(request, "encyclopedia/index.html", {"entries": entries})
 
 
 def title(request, title):
-    entries = util.list_entries()
     up_entries = [entry.upper() for entry in entries]
     if title.upper() in up_entries:
         content = markdown2.markdown(util.get_entry(title))
